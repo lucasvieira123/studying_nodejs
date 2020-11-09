da3f13a3b0ba154ce4cdc235a1551d5dbb6bc07b
lucas <lucas.vieira@aluno.uece.br>
2020-11-09 16:44:49 -0300

    initial studys about nodejs

---
+++ stats.js
@@ -0,0 +1,26 @@
+const os = require("os")
+
+const log =require("./logger")
+setInterval(() => {
+
+    const {freemem, totalmem} = os
+
+    const total = parseInt(totalmem()/1024/1024)
+    const mem = parseInt(freemem()/1024/1024)
+    const percents = parseInt((mem / total)*100)
+    
+    console.log(mem, total, percents)
+    
+    const stats = {
+        free:`${mem} MB`,
+        total: `${total} MB`,
+        usage: `${percents} MB`
+    }
+    
+    console.clear()
+    console.log("=== PC STATS ===")
+    console.table(stats)
+
+    log(`${JSON.stringify(stats)}\n`)
+
+}, 1000)
